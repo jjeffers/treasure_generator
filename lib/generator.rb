63592fc8fff2fb2e07409470e306e88e90e5a80b
James Jeffers <jdjeffers@gmail.com>
2014-09-08 20:31:43 -0400

    Adding jeweled items.

--- lib/generator.rb
+++ lib/generator.rb
@@ -169,7 +169,7 @@ module Treasure
 		when 13..17
 			["religious",0]
 		when 18..19
-			["wealthy/nobel",1]
+			["wealthy/noble",1]
 		when 20
 			["royalty", 2]
 		end
@@ -196,6 +196,125 @@ module Treasure
 		end
 	end
 
+	def self.art_quality(roll)
+		case roll
+		when 1..5
+			["awful quality",-3]
+		when 5..10
+			["poor quality",-2]
+		when 11..25
+			["below average quality",-1]
+		when 26..65
+			["average quality",0]
+		when 66..85
+			["above average quality",1]
+		when 86..90
+			["good quality",2]
+		when 91..95
+			["excellent quality",3]
+		when 96..98
+			["finest quality",4]
+		when 99..100
+			["unqique quality", 5]
+		end
+	end
+
+	def self.art_age(roll)
+		case roll
+		when 1..10
+			["avante-garde", -3]
+		when 11..20
+			["current", -2]
+		when 21..50
+			["recent", -1]
+		when 51..63
+			["contemporary", 0]
+		when 64..73 
+			["modern", 1]
+		when 74..82
+			["old", 2]
+		when 83..90
+			["antique", 3]
+		when 91..95
+			["venerable",4]
+		when 96..97
+			["archaic", 5]
+		when 98..99
+			["antedeluvian",6]
+		when 100
+			["primordial", 7]
+		end
+	end
+
+	def self.art_size(roll)
+		case roll
+		when 1..5
+			["tiny",-3]
+		when 5..10
+			["very small",-2]
+		when 11..25
+			["small",-1]
+		when 26..65
+			["average",0]
+		when 66..85
+			["large",1]
+		when 86..90
+			["very large",2]
+		when 91..95
+			["huge",3]
+		when 96..98
+			["massive",4]
+		when 99..100
+			["guargantuan", 5]
+		end
+	end
+
+	def self.art_craftmanship(roll)
+		case roll
+		when 1..5
+			["awful craftsmanship",-3]
+		when 5..10
+			["poor craftsmanship",-2]
+		when 11..25
+			["below average craftsmanship",-1]
+		when 26..65
+			["average craftsmanship",0]
+		when 66..85
+			["above average craftsmanship",1]
+		when 86..90
+			["good craftsmanship",2]
+		when 91..95
+			["excellent craftsmanship",3]
+		when 96..98
+			["brilliant craftsmanship",4]
+		when 99..100
+			["masterpiece craftsmanship", 5]
+		end
+	end
+
+	def self.art_condition(roll)
+		case roll
+		when 1..5
+			["badly damaged",-3]
+		when 5..10
+			["damaged",-2]
+		when 11..25
+			["worn",-1]
+		when 26..65
+			["average",0]
+		when 66..85
+			["good condition",1]
+		when 86..90
+			["excellent condition",2]
+		when 91..95
+			["near perfect conidtion",3]
+		when 96..98
+			["perfect condition",4]
+		when 99..100
+			["flawless condition", 5]
+		end
+	end
+
 	def self.generate_art_treasure(gp_size)
 
 		art = { :type => "art", :descriptions => [], :value => 0}
@@ -209,6 +328,26 @@ module Treasure
 		artist = artist_reknown(roll("1d100"))
 		art[:descriptions] << artist[0]
 		value_adjustment += artist[1]
+
+		quality = art_quality(roll("1d100"))
+		art[:descriptions] << quality[0]
+		value_adjustment += quality[1]
+
+		age = art_age(roll("1d100"))
+		art[:descriptions] << age[0]
+		value_adjustment += age[1]
+
+		size = art_size(roll("1d100"))
+		art[:descriptions] << size[0]
+		value_adjustment += size[1]
+
+		craftsmanship = art_craftmanship(roll("1d100"))
+		art[:descriptions] << craftsmanship[0]
+		value_adjustment += craftsmanship[1]
+
+		condition = art_condition(roll("1d100"))
+		art[:descriptions] << condition[0]
+		value_adjustment += condition[1]
 
 		art_value_index = @@art_values.find_index(
 			@@art_values.min { |a,b| (a-gp_size).abs <=> (b-gp_size).abs })
@@ -235,6 +374,153 @@ module Treasure
 	end
 
 	def self.generate_jeweled_items_treasure(gp_size)
+
+		value_adjustment = 0
+		jeweled_item = { :type => "jeweled item", :descriptions => [], :value => gp_size}
+
+		jeweled_item[:descriptions] << case roll("1d100")
+		when 1..2
+			"anklet"
+		when 3..5
+			"armband"
+		when 6..7
+			"small box"
+		when 8..9
+			"belt"
+		when 10..14
+			"bracelet"
+		when 15..17
+			"brooch"
+		when 18
+			"buckle"
+		when 19
+			"chain"
+		when 20
+			"chalice"
+		when 21
+			"choker"
+		when 22
+			"clasp"
+		when 23
+			"collar"
+		when 24
+			"coffer"
+		when 25
+			"comb"
+		when 26
+			"coronet"
+		when 27
+			"crown"
+		when 28
+			"decanter"
+		when 29
+			"diadem"
+		when 30-32
+			"earring"
+		when 33
+			"fob"
+		when 34..35
+			"goblet"
+		when 36..37
+			"headband"
+		when 38
+			"idol"
+		when 39..40
+			"locket"
+		when 41
+			"medal"
+		when 42
+			"medallion"
+		when 43..45
+			"necklace"
+		when 46
+			"pendant"
+		when 47
+			"pin"
+		when 48
+			"orb"
+		when 49..53
+			"ring"
+		when 54
+			"scepter"
+		when 55
+			"seal"
+		when 56
+			"statuette"
+		when 57
+			"tiara"
+		when 58..59
+			"mask"
+		when 60
+			"nose ring/stud"
+		when 61..62
+			"circlet"
+		when 63..64
+			"torc"
+		when 65..67
+			"utensils"
+		when 68
+			"chatelaine"
+		when 69
+			"cuff link"
+		when 70
+			"lapel pin"
+		when 71
+			"grill"
+		when 72
+			"bangles"
+		when 73
+			"body piercing"
+		when 74
+			"prayer beads"
+		when 75
+			"puzzle"
+		when 76
+			"aguilette"
+		when 77
+			"cock ring"
+		when 78
+			"pectoral"
+		when 79
+			"zerscheibe"
+		when 80
+			"icon"
+		when 81
+			"egg"
+		when 82..85
+			"weapon"
+		when 86..89 
+			"armor"
+		when 90..93
+			"tool"
+		when 94..99
+			bejewled_item_type = "coins"
+			while bejewled_item_type == "coins"
+				bejewled_item_type = treasure_type(roll("1d20"))
+			end
+			bejewled_item_type
+		when 100
+			"container is bejeweled"
+		end
+
+		quality = art_quality(roll("1d100"))
+		jeweled_item[:descriptions] << quality[0]
+		value_adjustment += quality[1]
+
+		craftsmanship = art_craftmanship(roll("1d100"))
+		jeweled_item[:descriptions] << craftsmanship[0]
+		value_adjustment += craftsmanship[1]
+
+		condition = art_condition(roll("1d100"))
+		jeweled_item[:descriptions] << condition[0]
+		value_adjustment += condition[1]
+
+		value_index = @@art_values.find_index(
+			@@art_values.min { |a,b| (a-gp_size).abs <=> (b-gp_size).abs })
+		
+		value_index += value_adjustment
+		jeweled_item[:value] = @@art_values[value_index]
+		jeweled_item
 	end
 
 	def generate(treasure_units=1, treasure_unit_size=200)
